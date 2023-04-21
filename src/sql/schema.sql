a01690db345c4adf321ce8c4ff6f841ed6e2d5a6
Gooogr <gusarovga@ya.ru>
2023-04-21 23:12:32 +0300

    feat: minor code update

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE Rating (
+CREATE TABLE ratings (
   user_id INT,
   isbn VARCHAR(255),
   rating INT
 );
 
 LOAD DATA INFILE '/data/ratings.csv'
-INTO TABLE Rating
+INTO TABLE ratings
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-ALTER TABLE Rating ADD column `id` INT unsigned primary KEY AUTO_INCREMENT FIRST;
+ALTER TABLE ratings ADD column `id` INT unsigned primary KEY AUTO_INCREMENT FIRST;
